bf591db4f3373267df95bffda325fd13142db6eb
ahmet794 <ahmet794>
2024-11-12 16:31:57 +0000

    Added comments on Data Modelling - SCD Type2.py

--- ADE 2 - Streaming ETL Patterns with DLT/Pipeline/ADE 2.5.1 - Data Modeling - SCD Type 2.py
+++ ADE 2 - Streaming ETL Patterns with DLT/Pipeline/ADE 2.5.1 - Data Modeling - SCD Type 2.py
@@ -20,47 +20,64 @@ import pyspark.sql.functions as F
 
 # COMMAND ----------
 
+# Assign a string to user_schema for easier use later on
 users_schema = "user_id LONG, update_type STRING, timestamp FLOAT, dob STRING, sex STRING, gender STRING, first_name STRING, last_name STRING, address STRUCT<street_address: STRING, city: STRING, state: STRING, zip: INT>"    
 
+# Create the DLT table in the pipeline
 @dlt.table(
     table_properties={"quality": "bronze"}
 )
+# By definining this function, give the table a name, do transformation in the return statement
 def users_cdc_bronze():
     return (
         dlt.read_stream("bronze")
+        # Read from the Bronze table
           .filter("topic = 'user_info'")
+          # Filter by the topic called 'user_info'
           .select(F.from_json(F.col("value").cast("string"), users_schema).alias("v"))
+          # Parse the value column cast it to a string and select the columns from users_schema
           .select("v.*")
+          # Expands all fields within the v column, making each field in the JSON structure a top-level column in the resulting DataFrame
     )
 
 # COMMAND ----------
 
+# Assign rules for future use.
 rules = {
   "valid_user_id": "user_id IS NOT NULL",
   "valid_operation": "update_type IS NOT NULL"
 }
 
+# Create the DLT table.
 @dlt.table(
     table_properties={"quality":"bronze"}
 )
+# Apply data quality rules using the expect_all_or_drop
 @dlt.expect_all_or_drop(rules)
+# Create the table named users_cdc_clean where will be the first step to SCD Type 2.
 def users_cdc_clean():
     return (
-        dlt.read_stream("users_cdc_bronze")        
+        dlt.read_stream("users_cdc_bronze")  
+        # Apply SQL transformations      
             .select(
                 # Adding a pseudonymized key to incremental workloads is as simple as adding a transformation.
 #                 F.sha2(F.concat(F.col("user_id"), F.lit(salt)), 256).alias("alt_id"),
-                F.col("user_id"),              
+                F.col("user_id"),    
                 F.col("timestamp").cast("timestamp").alias("updated"),
+                # Cast timestamp column as updated
                 F.to_date("dob", "MM/dd/yyyy").alias("dob"), 
                 "sex", "gender", "first_name", "last_name", "address.*", "update_type")
+                # Change the format of dob column and SELECT the rest of the columns
     )
 
+# Quarantine rules are defined
 quarantine_rules = {}
-quarantine_rules["invalid_record"] = f"NOT({' AND '.join(rules.values())})"
+quarantine_rules["invalid_record"] : f"NOT({' AND '.join(rules.values())})"
+# Quarantine rules are created by joining them with the values from rules that was defined early on. If the data does not follow the rules, then it will marked as invalid record inside the quarantine_rules dictionary.
 
 @dlt.table
 @dlt.expect_all_or_drop(quarantine_rules)
+# Create the users_cdc_quarantine DLT, reading from the bronze table for this specific data. (users_cdc_bronze)
 def users_cdc_quarantine():
     return (
         dlt.read_stream("users_cdc_bronze")
@@ -82,7 +99,7 @@ dlt.apply_changes(
   except_column_list = ["update_type"]
 )
 
-
+# Here SCD2 is applied. DLT has an easy way of CDC and applying SCD using the apply_changes function.
 dlt.create_streaming_live_table(
   name="SCD2_users", 
   table_properties={"quality": "silver"}
@@ -94,7 +111,7 @@ dlt.apply_changes(
   sequence_by = F.col("updated"),
   apply_as_deletes = F.expr("update_type = 'delete'"),
   except_column_list = ["update_type"],
-  stored_as_scd_type = "2" #Enable SCD2 and store individual updates
+  stored_as_scd_type = "2" #Enable SCD2 and store individual updates. When this parameter is omitted then apply_changes assumes an SCD Type 1 like behaviour.
 )
 
 # COMMAND ----------
