4118cacbbf3a0b07b031e715c0f560d8d23b7fa5
ahmet794 <ahmet794>
2024-11-13 14:55:42 +0000

    Added Solutions Quarantine.py Lab

--- ADE 5 - SWE Patterns for DLT/Pipeline/silver/quarantine.py
+++ ADE 5 - SWE Patterns for DLT/Pipeline/silver/quarantine.py
@@ -33,11 +33,14 @@ import pyspark.sql.functions as F
 # COMMAND ----------
 
 # TODO
-lookup_db = spark.FILL_IN
+lookup_db = spark.conf.get('lookup_db')
 
 def get_rules(topic):
-    df = spark.FILL_IN
-)
+    df = spark.read.table(f"{lookup_db}.rules").filter(F.col("topic") == topic)
+    rules = {}
+    for row in df.collect():
+        rules[row["name"]] = row["condition"]
+    return rules
 
 # COMMAND ----------
 
@@ -55,6 +58,26 @@ def get_rules(topic):
 # COMMAND ----------
 
 # TODO
+def create_validated_views(dataset, topic, source_table, valid_view, invalid_view):
+  rules = get_rules(topic)
+  quarantine_rules = "NOT({0})".format(" AND ".join(rules.values()))
+
+  @dlt.table(
+    name = f"{dataset}_quarantine",
+    partition_cols = ["is_quarantined"]
+  )
+
+  @dlt.expect_all(rules)
+  def create_quarantine():
+    return dlt.read_stream(source_table).withColumn("is_quarantined", F.expr(quarantine_rules))
+  
+  @dlt.view(name=f"{valid_view}")
+  def create_valid():
+    return dlt.read_stream(f"{dataset}_quarantine").filter("is_quarantined = false")
+  
+  dlt.view(name=f"{invalid_view}")
+  def create_invalid():
+    return dlt.read_stream(f"{dataset}_quarantine").filter("is_quarantined = true")
 
 # COMMAND ----------
 
@@ -75,6 +98,25 @@ def get_rules(topic):
 # TODO
 
 quarantine_tables_config = {
-    FILL_IN
-}
+    "bpm": { 
+      "rules_tag": "bpm",
+      "source": "bpm_bronze",
+      "valid_view": "valid_bpm",
+      "invalid_view": "invalid_bpm"
+    },
+    "workouts": { 
+      "rules_tag": "workout",
+      "source": "workouts_bronze",
+      "valid_view": "valid_workouts",
+      "invalid_view": "invalid_workouts"
+    },
+    "users_cdc": { 
+      "rules_tag": "user_info",
+      "source": "users_cdc_bronze",
+      "valid_view": "valid_users_cdc",
+      "invalid_view": "invalid_users_cdc"
+    }
+} 
 
+for dataset, c in quarantine_tables_config.items():
+    create_validated_views(dataset, c["rules_tag"], c["source"], c["valid_view"], c["invalid_view"])
